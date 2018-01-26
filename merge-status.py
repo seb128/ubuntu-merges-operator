3cff6fdeacdfe4e9cf9a60c5ae929ed556450caf
Julian Andres Klode <juliank@ubuntu.com>
2018-01-26 17:49:05 +0100

    merge-status.py: Generate case-insensitive filter

--- merge-status.py
+++ merge-status.py
@@ -304,11 +304,12 @@ def write_status_page(component, merges, left_distro, right_distro):
 
                     // Function to filter stuff
                     function filterText() {
+                        var regexp=new RegExp(query.value, "i");
                         var tables=document.getElementsByTagName("table");
                         for (var t=0; t < tables.length; t++) {
                             var rows=tables[t].getElementsByTagName("tr");
                             for (var i=2; i < rows.length; i += 2)  {
-                                var hide = (query.value && !rows[i].textContent.match(query.value)) ||
+                                var hide = (query.value && !rows[i].textContent.match(regexp)) ||
                                            (!showProposed.checked && rows[i].bgColor === '#d0d0d0') ||
                                            (!showMergeNeeded.checked && rows[i].bgColor !== '#d0d0d0');
                                 rows[i].hidden=rows[i+1].hidden=hide;
