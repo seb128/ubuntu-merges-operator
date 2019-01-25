794ba986c457d5626b13a780daa9fb733ed48860
Colin Watson <cjwatson@canonical.com>
2019-01-25 18:09:21 +0000

    Silence flake8 "imported but unused" messages.

--- deb/__init__.py
+++ deb/__init__.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__all__ = ["controlfile", "source", "version"]
+
 from deb import controlfile
 from deb import source
 from deb import version
