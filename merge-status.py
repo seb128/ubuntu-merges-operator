6e0e3ee4be5d18a125f59f03f958de82cc948b41
Colin Watson <cjwatson@canonical.com>
2019-06-04 23:57:27 +0100

    Use HTTPS for links to people.canonical.com.

--- merge-status.py
+++ merge-status.py
@@ -629,7 +629,7 @@ else:\n\
         print("<td><small>%s</small></td>" % source["Binary"], file=status)
         if proposed_version:
             excuses_url = (
-                "http://people.canonical.com/~ubuntu-archive/"
+                "https://people.canonical.com/~ubuntu-archive/"
                 "proposed-migration/update_excuses.html"
             )
             print(
