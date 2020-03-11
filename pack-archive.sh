003b5a136f363a8885c0758f39c1730f233a7538
Colin Watson <cjwatson@canonical.com>
2020-03-11 09:03:02 +0000

    pack-archive.sh: Run bzip2 with -q.

--- pack-archive.sh
+++ pack-archive.sh
@@ -7,6 +7,6 @@ MOMLIB_ROOT=/srv/patches.ubuntu.com
 
 cd $MOMLIB_ROOT
 
-find changes -name "*.changes" -mtime +182 -print0 | xargs -0r bzip2
-find diffs -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2
-find patches \( -name "*.patch" -o -name "*.slipped-patch" \) -mtime +182 -print0 | xargs -0r bzip2
+find changes -name "*.changes" -mtime +182 -print0 | xargs -0r bzip2 -q
+find diffs -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2 -q
+find patches \( -name "*.patch" -o -name "*.slipped-patch" \) -mtime +182 -print0 | xargs -0r bzip2 -q
