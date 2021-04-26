7718dcc73bc0d267ff43927946a8a51641a2f119
Brian Murray <brian@canonical.com>
2021-04-26 11:21:46 -0700

    switch to impish

--- momlib.py
+++ momlib.py
@@ -53,7 +53,7 @@ ROOT = "/srv/patches.ubuntu.com"
 DISTROS = {
     "ubuntu": {
         "mirror": "http://archive.ubuntu.com/ubuntu",
-        "dists": ["hirsute"],
+        "dists": ["impish"],
         "components": ["main", "restricted", "universe", "multiverse"],
         "expire": True,
     },
@@ -96,7 +96,7 @@ DISTROS = {
 
 # Destination distribution and release
 OUR_DISTRO = "ubuntu"
-OUR_DIST = "hirsute"
+OUR_DIST = "impish"
 
 # Default source distribution and release
 SRC_DISTRO = "debian"
