7260042d327efb2b1dcfd99ca9d7061caad5979d
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2024-05-07 11:03:01 +0200

    switch to oracular

--- momlib.py
+++ momlib.py
@@ -59,7 +59,7 @@ ROOT = "/srv/patches.ubuntu.com"
 DISTROS = {
     "ubuntu": {
         "mirror": "http://archive.ubuntu.com/ubuntu",
-        "dists": ["noble"],
+        "dists": ["oracular"],
         "components": ["main", "restricted", "universe", "multiverse"],
         "expire": True,
     },
@@ -102,7 +102,7 @@ DISTROS = {
 
 # Destination distribution and release
 OUR_DISTRO = "ubuntu"
-OUR_DIST = "noble"
+OUR_DIST = "oracular"
 
 # Default source distribution and release
 SRC_DISTRO = "debian"
