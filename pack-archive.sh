26505aa1e07b61692dd10bc51ae8f2681fbac2de
Florent 'Skia' Jacquet <florent.jacquet@canonical.com>
2025-02-17 15:41:04 +0100

    Please shellcheck a bit more

--- pack-archive.sh
+++ pack-archive.sh
@@ -5,7 +5,7 @@ umask 002
 
 MOMLIB_ROOT=/srv/patches.ubuntu.com
 
-cd $MOMLIB_ROOT
+cd "$MOMLIB_ROOT"
 
 find changes -name "*.changes" -mtime +182 -print0 | xargs -0r bzip2 -q
 find diffs -name "*.patch" -mtime +182 -print0 | xargs -0r bzip2 -q
